961c4e0906e84efd7566f1bc6d2f2c2ce1f13924
zabi2404 <your_email@example.com>
2025-09-13 02:05:16 +0500

    login page completed

--- crm-get2uni/src/Pages/Auth/Login.tsx
+++ crm-get2uni/src/Pages/Auth/Login.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import { LoginForm } from "@/components/login-form"
+import { Button } from "@/components/ui/button"
 
-export default function Login() {
+export default function LoginPage() {
   return (
-    <div>Login</div>
+    <div className="flex flex-col h-screen">
+     
+      <div className="flex justify-end p-4 pt-10 pr-10">
+        <Button  to="/signup" className="cursor-pointer">Sign Up</Button>
+      </div>
+
+     
+      <div className="flex flex-1 items-center justify-center">
+        <div className="w-full max-w-md min-[1440px]:max-w-[600px] transition-all duration-200">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
   )
 }
