99192a43751ce8a6c8ea8cb6f27a324cbc80b613
zabi2404 <your_email@example.com>
2025-09-16 00:34:36 +0500

    resolve conflict

--- crm-get2uni/src/App.tsx
+++ crm-get2uni/src/App.tsx
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-import toast, { Toaster } from "react-hot-toast";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-import TopBar from "./components/common/TopBar";
-import { useEffect } from "react";
-import {  useTheme } from "./Zustand/themeSwitcherStore";
-import i18n from "./i18n";
-import { useLanguage } from "./Zustand/LanguageSwitcherStore";
-function App() {
-
-  const {theme} = useTheme();
-  const {language} = useLanguage();
-  useEffect(() => {
-   
-    if(theme==='dark'){
-      document.documentElement.classList.add('dark')
-    }else{
-      document.documentElement.classList.remove('dark')
-    }
-    
-    if(language === 'French'){
-      i18n.changeLanguage("fr")
-      return
-  }else{
-
-    i18n.changeLanguage("en")
-  }
-  }, [theme,language]);
-
-
-
-
-  // 
-
-  // const theme = ()=>{
-  //   console.log('working');
-  //   document.documentElement.classList.add("dark")
-  // }
-
-  // const theme1 = ()=>{
-  //   console.log('working');
- 
-  //   document.documentElement.classList.remove("dark")
-  // }
-
-  // const calltoast = () => {
-  //   {
-
-  //     toast('Hello Darkness!',
-  //       {
-  //         icon: '👏',
-  //         style: {
-  //           borderRadius: '15px',
-  //           background: '#3393',
-  //           color: '#fff',
-  //         },
-  //       }
-  //     )
-  //   }
-
-  // }
-
-  return (
-    <>
-    <TopBar/>
-     <Toaster/>
-
-    <RouterProvider router={router}/>
-=======
 import  { Toaster } from "react-hot-toast"
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
@@ -87,7 +17,6 @@ function App() {
             
         </SidebarProvider>
      
->>>>>>> sidebar
     </>
   )
 }
