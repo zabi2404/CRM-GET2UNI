8699fb95de59b8f06e92de0f443ce072746a07b5
purple <839purple@tiffincrane.com>
2025-10-01 23:57:50 +0500

    most of work done minor detail and things to added also have to apply validation

--- crm-get2uni/src/components/common/ForgetPassword-form.tsx
+++ crm-get2uni/src/components/common/ForgetPassword-form.tsx
@@ -9,55 +9,52 @@ export function ForgetPasswordForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
-      const { t } = useTranslation();
+  const { t } = useTranslation();
 
 
-      const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState({});
 
 
-      const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 
-        setFormData({
-          ...formData,
-          [e.target.id]: e.target.value
-        })
-      }
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
 
 
-      const SubbmitForm = (e:React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault();
-        console.log(formData);
-      }
+  const SubbmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  }
   return (
     <form onSubmit={SubbmitForm} className={cn("flex flex-col gap-6 ", className)} {...props}>
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">{t('auth.forgotYourPassword')}</h1>
         <p className="text-muted-foreground text-sm text-balance">
-      {t('auth.forgotPasswordDescription')}
+          {t('auth.forgotPasswordDescription')}
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
-      
           <Input id="email"
-          onChange={handleChange}
-          type="email" placeholder="Email" required 
-           className="p-6"
+            onChange={handleChange}
+            type="email" placeholder="Email" required
+            className="p-6"
           />
         </div>
-    
         <Button type="submit" className="w-full cursor-pointer p-6">
           {t('auth.sendResetInstruction')}
         </Button>
- 
       </div>
       <div className="text-center text-sm">
-      <p>{t('auth.noAccount')}</p>
+        <p>{t('auth.noAccount')}</p>
         <Link to="/signup" className="text-primary ">
-       {t('auth.signupMessage')}
+          {t('auth.signupMessage')}
         </Link>
       </div>
-     
+
     </form>
   )
 }
